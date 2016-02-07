ad853569444dd6a5ae7c91500f3a90bd094fa368
sangress <shai.angress@gmail.com>
2016-02-07 21:32:21 +0200

    initial commit

---
+++ doshare.js
@@ -0,0 +1,29 @@
+Content = new Mongo.Collection("content");
+
+if (Meteor.isClient) {
+
+  Template.main.helpers({
+    contents: function () {
+      console.log(Content.find({id: 1}));
+      return Content.find({id: 1});
+    }
+  });
+
+  Template.content.events({
+    'blur textarea': function () {
+      console.log(this, event.target.value);
+      Content.update(this._id, {
+        $set: {text: event.target.value}
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    if (Content.find().count() === 0) {
+      Content.insert({id: 1, text: 'this is awesome!'});
+    }
+  });
+}
